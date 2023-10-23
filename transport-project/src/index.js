7f535d0cf38029d760da9efc960fd82a7606fc23
alopez94 <allan.u94l@gmail.com>
2023-10-23 14:33:49 -0600

    theme-installed

--- transport-project/src/index.js
+++ transport-project/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MantineProvider, createTheme } from '@mantine/core';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
 root.render(
   <React.StrictMode>
+   <MantineProvider theme={theme}>
     <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
