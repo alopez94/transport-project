8d0b3b02c6f0eed39c60700738d17baae89611d7
alopez94 <allan.u94l@gmail.com>
2023-10-30 15:05:03 -0600

    dashboard-layout

--- transport-project/src/pages/dashboard/Dashboard.js
+++ transport-project/src/pages/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Dashboard.module.css'
+import classes from'./Dashboard.module.css'
 import {Sidebar} from '../../components/Sidebar'
 import { Container, Grid, SimpleGrid, Skeleton, rem } from '@mantine/core';
 
@@ -10,17 +10,21 @@ export default function Dashboard() {
   const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
 
   return (
-    <div>
+    <div> 
       
-      <Container my="md">
-        
-      <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">       
       
-        <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false}> 
-          </Skeleton>
-        <Grid gutter="md">
-          <Grid.Col>
-            <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+      
+      <div className={classes.container} my="lg">   
+      <div className={classes.sidebar}>
+      <Sidebar /> 
+      </div>   
+      
+      <div className={classes.content}>
+      <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md"> 
+      <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />           
+        <Grid gutter="md">        
+          <Grid.Col> 
+          <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
           </Grid.Col>
           <Grid.Col span={6}>
             <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
@@ -30,8 +34,9 @@ export default function Dashboard() {
           </Grid.Col>
         </Grid>
       </SimpleGrid>
-    </Container>
-        
+      </div>
+    </div>               
+    
     </div>
   )
 }
