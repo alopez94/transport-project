2aa175d9640d5af8780e3b51c4190042b9f5c8bb
alopez94 <allan.u94l@gmail.com>
2023-11-07 01:16:24 -0600

    admin-tabs

--- transport-project/src/pages/admin/Admin.js
+++ transport-project/src/pages/admin/Admin.js
@@ -1,7 +1,66 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 export default function Admin() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>Admin</div>
-  )
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Transportistas"  />
+          <Tab label="Unidades"  />          
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+       Transportistas
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+       Unidades
+      </CustomTabPanel>
+      
+    </Box>
+  );
 }
